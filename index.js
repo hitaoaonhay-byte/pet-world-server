f2b792e502b84b2fc4b46497563286a38bb3fd15
hitaoaonhay-byte <hitaoaonhay@gmail.com>
2026-07-11 15:19:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Pet World API đang hoạt động!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server chạy cổng " + PORT);
+});
