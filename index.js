21abbd8822bcce15cb5e9a3710dd96babf89bb04
hitaoaonhay-byte <hitaoaonhay@gmail.com>
2026-07-12 10:16:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,13 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Pet World API đang hoạt động!");
 });
-
+pool.query("SELECT NOW()")
+.then(() => {
+    console.log("Đã kết nối PostgreSQL");
+})
+.catch(err => {
+    console.error("Lỗi PostgreSQL:", err);
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
